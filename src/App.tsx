16c703459e61664c4d98754243242c23ae9c17aa
shamiljafarov <shamildjafarli07@gmail.com>
2025-02-21 06:05:46 +0400

    express added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./shared/header/header";
-import HomePage from "./pages/home_page/home_page";
+import HomePage from "./pages/home/home_page";
 import Footer from "./shared/footer/footer";
+import ExpressPage from "./pages/express/express_page"
 
 function App() {
-  return (
-    <Router>
-      <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      <Footer />
-    </Router>
-  );
+	return (
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/express" element={<ExpressPage />} />
+			</Routes>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
